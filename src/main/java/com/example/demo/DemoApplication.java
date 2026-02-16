5bf458406b59f41415e2fa63ba9d6624395a6435
Ramya-2709 <ramyakokku49@gmail.com>
2026-02-16 14:05:38 +0530

    Adding the new service class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-	//main class for the application
+	//main class
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
